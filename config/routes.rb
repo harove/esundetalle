80983c049519e10be4d62f44e43c8cef98847eed
edgar <harove@gmail.com>
2019-07-05 10:52:34 -0400

    e-commerce base ok

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,17 @@ Rails.application.routes.draw do
   	end
   end
 
+  resources :billings, only: [:index] do
+    collection do
+      get 'pre_pay'
+      get 'execute'
+    end
+  end
+
+
+
+
+
   devise_for :users
 
   root 'products#index'
