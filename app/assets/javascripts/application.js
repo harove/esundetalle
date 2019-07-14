64c2e2126b0968df875e904ae0136428f8eb970c
edgar <harove@gmail.com>
2019-07-14 18:15:33 -0400

    shared yaml navbar ok. Jquery issue solved installing yarn and giving priority to jquery in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= requiere jquery/dist/jquery
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
